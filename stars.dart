6a016ee6e289fbdb71c138d8c4a08331359f523c
om7-sa <76610233+om7-sa@users.noreply.github.com>
2023-05-02 18:20:39 +0300

    Revert "solve: stars challenge solution"

--- stars.dart
+++ stars.dart
@@ -1,13 +1,3 @@
 void main() {
-  String star = '*';
-
-  for (int i = 1; i <= 9; i++) {
-    print(star);
-
-    if (i <= 4) {
-      star += '*';
-    } else {
-      star = star.substring(0, star.length - 1);
-    }
-  }
+print (hi);
 }
